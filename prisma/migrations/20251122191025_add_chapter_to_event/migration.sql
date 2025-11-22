8e49e34ccc5d6179b83b0d6f906aa71db63dba18
legendy4141 <legendy4141@gmail.com>
2025-11-22 14:18:25 -0500

    feat: add chapter schema to event schema

---
+++ prisma/migrations/20251122191025_add_chapter_to_event/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "Event" ADD COLUMN     "chapterId" TEXT;
+
+-- CreateTable
+CREATE TABLE "Chapter" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "emoji" TEXT NOT NULL,
+
+    CONSTRAINT "Chapter_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_chapterId_fkey" FOREIGN KEY ("chapterId") REFERENCES "Chapter"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Chapter_name_key" ON "Chapter"("name");
