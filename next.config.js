7ca16c449c924adc5a2654470905a07709d50dda
Divv Saxena <111836493+DivvSaxena@users.noreply.github.com>
2025-11-22 14:26:34 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-import createNextIntlPlugin from "next-intl/plugin";
-
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
 await import("./src/env.js");
-
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -20,4 +16,4 @@ const config = {
   },
 };
 
-export default withNextIntl(config);
+export default config;
